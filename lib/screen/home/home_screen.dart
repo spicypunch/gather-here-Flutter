c1bd8d4f345cb50440f94d89f6bebb2a885f086d
DuDu <whqtkf12@naver.com>
2024-08-14 13:27:12 +0900

    Feature: DefaultLayout 구현 (#10)

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'package:gather_here/common/components/default_layout.dart';
+import 'package:go_router/go_router.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          '홈스크린',
-          style: TextStyle(
-            fontWeight: FontWeight.w900,
-            fontSize: 40,
-          ),
-        ),
+    return DefaultLayout(
+      title: 'Home',
+      backgroundColor: Colors.red,
+      appBarBackgroundColor: Colors.green,
+      child: Center(
+        child: ElevatedButton(onPressed: (){
+          context.pushNamed('home');
+        }, child: Text('Push')),
       ),
     );
   }
