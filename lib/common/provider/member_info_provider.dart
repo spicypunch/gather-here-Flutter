b576d98d32d06a75d532de6f5216cf8e351dc4ec
DuDu <whqtkf12@naver.com>
2024-10-17 18:58:52 +0900

    [D-0] myPage 개선 (#37)
    * refactor: MyPage 코드정리
    
    * feat: 서비스 이용약관 링크 추가

--- lib/common/provider/member_info_provider.dart
+++ lib/common/provider/member_info_provider.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gather_here/common/model/request/nickname_model.dart';
 import 'package:gather_here/common/model/response/member_info_model.dart';
 import 'package:gather_here/common/repository/member_repository.dart';
 
@@ -12,9 +13,11 @@ import '../model/response/profile_image_url_model.dart';
 
 class MemberInfoState {
   MemberInfoModel? memberInfoModel;
+  String? message;
 
   MemberInfoState({
     this.memberInfoModel,
+    this.message,
   });
 }
 
@@ -41,16 +44,36 @@ class MemberInfoProvider extends StateNotifier<MemberInfoState> {
     getMyInfo();
   }
 
+  void _setState() async {
+    state = MemberInfoState(memberInfoModel: state.memberInfoModel, message: state.message);
+    await Future.delayed(Duration(seconds: 1));
+    state = MemberInfoState(memberInfoModel: state.memberInfoModel, message: null);
+  }
+
   Future<void> getMyInfo() async {
     try {
       final memberInfo = await memberRepository.getMemberInfo();
-      state = MemberInfoState(memberInfoModel: memberInfo);
+      state.memberInfoModel = memberInfo;
+      _setState();
     } catch (e, stackTrace) {
       debugPrint('getMyInfo: $e, $stackTrace');
     }
   }
 
-  Future<bool> changeProfileImage(File imageFile) async {
+  Future<void> changeNickName(String nickName) async {
+    try {
+      await memberRepository.patchChangeNickName(
+        body: NicknameModel(nickname: nickName),
+      );
+      state.message = '닉네임 변경에 성공하였습니다';
+      getMyInfo();
+    } catch (e) {
+      state.message = '닉네임 변경에 실패하였습니다.';
+      _setState();
+    }
+  }
+
+  void changeProfileImage(File imageFile) async {
     try {
       final fileName = imageFile.path.split('/').last;
 
@@ -76,23 +99,25 @@ class MemberInfoProvider extends StateNotifier<MemberInfoState> {
       if (response.statusCode == 200) {
         final profileImageUrlModel =
             ProfileImageUrlModel.fromJson(response.data);
-        state = MemberInfoState(
-          memberInfoModel: state.memberInfoModel
-              ?.copyWith(profileImageUrl: profileImageUrlModel.imageUrl),
-        );
-        return true;
+
+        state.message = '프로필 사진이 업데이트 되었습니다.';
+        state.memberInfoModel = state.memberInfoModel?.copyWith(profileImageUrl: profileImageUrlModel.imageUrl);
+        _setState();
       } else {
         debugPrint('Server responded with status code: ${response.statusCode}');
-        return false;
+        state.message = '프로필 사진 업데이트에 실패하였습니다.';
+        _setState();
       }
     } catch (e) {
+      state.message = '프로필 사진 업데이트에 실패하였습니다.';
+      _setState();
+
       if (e is DioException) {
         debugPrint('changeProfileImage Dio Error: ${e.message}');
         debugPrint('Response: ${e.response?.data}');
       } else {
         debugPrint('changeProfileImage Unknown Error: $e');
       }
-      return false;
     }
   }
 }
